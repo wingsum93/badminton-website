7929826adff27098a7cffd2e27f2eb2324e322af
Eric Ho <wingsum.developer@gmail.com>
2026-09-14 12:04:01 +0800

    Add sitemap, add 1 version

--- scripts/build-sites.mjs
+++ scripts/build-sites.mjs
@@ -17,6 +17,7 @@ const files = [
   "pages/tactics.html",
   "pages/training.html",
   "pages/wipe-power.html",
+  "sitemap.xml",
   "assets/css/styles.css",
   "assets/js/main.js",
 ];
@@ -25,6 +26,7 @@ const contentTypes = {
   ".css": "text/css; charset=utf-8",
   ".html": "text/html; charset=utf-8",
   ".js": "text/javascript; charset=utf-8",
+  ".xml": "application/xml; charset=utf-8",
 };
 
 const assets = Object.fromEntries(
